12776618734dd522ca89f2700f0dc9008af030fe
* <veetube.jo@gmail.com>
2026-02-19 00:37:06 +0300

    Configure AndroidX SplashScreen: update manifest, styles.xml, build.gradle, and initialize in MainActivity

--- android/app/src/main/java/com/exponetflix/MainActivity.java
+++ android/app/src/main/java/com/exponetflix/MainActivity.java
@@ -2,22 +2,31 @@ package com.exponetflix;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 import expo.modules.ReactActivityDelegateWrapper;
+import androidx.core.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    // Install SplashScreen before React Native sets up the UI
+    SplashScreen.installSplashScreen(this);
+
+    // Keep Expo’s expected behavior (null instead of savedInstanceState)
     super.onCreate(null);
   }
 
   @Override
   protected String getMainComponentName() {
-    return "main"; // This should match the "name" in your index.js AppRegistry.registerComponent
+    return "main"; // This should match the "name" in AppRegistry.registerComponent in index.js
   }
 
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this, new ReactActivityDelegate(this, getMainComponentName()));
+    return new ReactActivityDelegateWrapper(
+      this,
+      new ReactActivityDelegate(this, getMainComponentName())
+    );
   }
 }
